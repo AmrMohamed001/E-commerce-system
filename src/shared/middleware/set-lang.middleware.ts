1b9b7ddb4e82637061cc5fbd3ae90dc13af48ac9
AmrMohamed001 <amrmohamd229@gmail.com>
2024-08-01 22:47:42 +0300

    extra check for language system

--- src/shared/middleware/set-lang.middleware.ts
+++ src/shared/middleware/set-lang.middleware.ts
@@ -1,4 +1,8 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	NestMiddleware,
+} from '@nestjs/common';
 import { Response, NextFunction } from 'express';
 import { Request } from '../interfaces/custom-request.interface';
 import { LanguagesService } from 'src/languages/languages.service';
@@ -6,9 +10,17 @@ import { LanguagesService } from 'src/languages/languages.service';
 export class SetLangMiddleware implements NestMiddleware {
 	constructor(private langService: LanguagesService) {}
 	async use(req: Request, res: Response, next: NextFunction) {
+		const isLangExists = await this.langService.findByLanguage(
+			req.headers['lang'] as string
+		);
+		if (!isLangExists)
+			return next(
+				new BadRequestException(
+					'this language is not supported,please provide correct one '
+				)
+			);
 		const defaultLang = await this.langService.findDefault();
 		let lang = req.headers['lang'] || defaultLang.language;
-		
 
 		req.i18nLang = lang as string;
 		next();
